bef76c9e245620b4600510e88fbbbdab1a4a3dcb
Mateusalyyy <profissionalmps1@gmail.com>
2025-08-18 15:13:00 -0300

    Create slab.py

---
+++ api/models/slab.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+class Slab(Base):
+    __tablename__ = "slabs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    material_type = Column(String, index=True)
+    total_area_m2 = Column(Float, nullable=False)
+    available_area_m2 = Column(Float, nullable=False)
+    price_per_m2 = Column(Float, nullable=False)
+    
+    # Futuramente, quando tivermos a tabela 'companies'
+    # company_id = Column(Integer, ForeignKey("companies.id"))
+    # company = relationship("Company", back_populates="slabs")
