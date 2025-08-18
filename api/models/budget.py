876fd6febb19a32751710e1c9a6bbf0bace8fe44
Mateusalyyy <profissionalmps1@gmail.com>
2025-08-18 16:19:08 -0300

    Update budget.py

--- api/models/budget.py
+++ api/models/budget.py
@@ -3,7 +3,7 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 import uuid
-from ..database import Base
+from ..db_base import Base
 
 
 class Budget(Base):
