1e07d876d4d8443db6c792dcdb6453170b138281
Mateusalyyy <profissionalmps1@gmail.com>
2025-08-18 16:19:30 -0300

    Update slab.py

--- api/models/slab.py
+++ api/models/slab.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from ..database import Base
+from ..db_base import Base
 
 class Slab(Base):
     __tablename__ = "slabs"
